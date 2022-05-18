728e77145c997967d2af87a9450d855d0ec05383
seescoto <sescoto@gmu.edu>
2022-05-17 21:28:43 -0400

    working on toString

--- WordSearch.java
+++ WordSearch.java
@@ -1,14 +1,20 @@
-//sofia escoto
-//makes a word search grid that can be printed out and adds whatever words given
+/**
+sofia escoto
+makes a word search grid that can be printed out and/or put into a txt file
+gives whatever words given
+*/
+
 
 public class WordSearch {
 
     private char[][] grid;
     priate final int MINSIZE = 10;
     private LinkedList<String> words;
-    private int maxWordSize;
+    private int maxWordSize = 0;
+    private String title = "Word Search"; //can be changed later
 
 
+    //constructor methods
     public WordSearch(int size) {
         grid = new char[size][size];
         words = new ArrayList<String>();
@@ -20,9 +26,21 @@ public class WordSearch {
         words = new ArrayList<String>();
     }
 
-    public setWordList(String[] words) {
+    public WordSearch(int size, String[] words) {
+        grid = new char[size][size];
+        words = new ArrayList<String>();
+        setWordList(words);
+    }
+
+    public WordSearch(String[] words) {
+        grid = new char[MINSIZE][MINSIZE];
+        words = new ArrayList<String>();
+        setWordList(words);
+    }
+
+    //enter an entire list of words
+    public void setWordList(String[] words) {
         //set initial word list
-        maxWordSize = 0;
         for (String w : words) {
             words.add(w);
             //if word is longer than all the ones before, change maxWordSize and make sure
@@ -30,12 +48,52 @@ public class WordSearch {
                 maxWordSize = w.length();
             }
         }
+        resize();
 
-        //make sure grid is big enough for all the words
+    }
+
+    //add words one by one
+    public void addToWordList(String newWord) {
+        words.add(newWord);
+        //check if need to change maxWordSize, if so resize
+        if (newWord.length() > maxWordSize) {
+            maxWordSize = newWord.length();
+            resize();
+        }
+
+    }
+
+    //makes the word search bigger if it isn't h x h where h = maxWordSize + 10
+    public void resize() {
         if (grid.length < (maxWordSize + 10)) {
             grid = new char[maxWordSize + 10][maxWordSize + 10];
         }
 
+    }
+
+
+    //returns string of char grid (formatted)
+    @Override
+    public String toString() {
+        String mssg = "";
+        int width = grid.length + 6;
+
+        //print top line
+        for (int i = 0; i < width; i++) {
+            mssg += "-";
+        }
+
+        //print all lines
+        for (int row = 0; row < grid.length; row++) {
+            mssg += "|" + "\t";
+            for (int col = 0; col < grid[row].length; col++) {
+                mssg += grid[row][length] + "\t";
+            }
+            mssg += "|" + "\n";
+        }
+
+        //print bottom line
+        for (int i)
     }
 
 
